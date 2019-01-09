989a87a497fb580edbdf4ca6b28f91b122cf45b2
Giorgio Brantzos <g.brantzos@gmail.com>
2019-01-09 16:56:07 +0200

    Using Font-Awesome instead of material icons.

--- src/app/core/components/main-nav/main-nav.component.ts
+++ src/app/core/components/main-nav/main-nav.component.ts
@@ -1,9 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
-import { Router, ActivationEnd } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material';
+import { Router, ActivationEnd } from '@angular/router';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'cubes-main-nav',
@@ -13,11 +12,6 @@ import { MatSidenav } from '@angular/material';
 export class MainNavComponent {
   public title = '';
   @ViewChild('drawer') public sidenav: MatSidenav;
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
 
   constructor(private breakpointObserver: BreakpointObserver, private router: Router) {
     // Also see: https://stackoverflow.com/a/52355983
