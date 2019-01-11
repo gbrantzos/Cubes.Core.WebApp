2a80765a9ca0b9e8fb5164ba1edfd99562ed7a0d
Giorgos Brantzos <g.brantzos@gmail.com>
2019-01-11 23:46:09 +0200

    Upgrade to Font-Awesome 5, enhanced about page.

--- src/app/core/components/about/about.component.ts
+++ src/app/core/components/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { VERSION } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'cubes-about',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  public ngVersion = VERSION.full;
+  public pingData$: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  ngOnInit() { this.pingServer(); }
 
-  ngOnInit() {
+  pingServer() {
+    this.pingData$ = this.http
+      .get('http://localhost:3001/api/core/ping')
+      .pipe(
+        map(res => (<any>res).result)
+      );
   }
-
 }
