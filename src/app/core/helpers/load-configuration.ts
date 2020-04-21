2bbe5fc5441e578e399f51453acbb2d6ac27cd0a
Giorgos Brantzos <g.brantzos@gmail.com>
2020-04-21 18:44:22 +0300

    Slight change on app layout

--- src/app/core/helpers/load-configuration.ts
+++ src/app/core/helpers/load-configuration.ts
@@ -1,47 +1,48 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ConfigurationService } from '@core/services/configuration.service';
-import { map, catchError, delay } from 'rxjs/operators';
-import { of, Observable, forkJoin } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
+import { catchError, delay, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
-export function loadConfiguration(http: HttpClient, configuration: ConfigurationService): (() => Promise<boolean>) {
+export function loadConfiguration(http: HttpClient, configuration: ConfigurationService): () => Promise<boolean> {
   return (): Promise<boolean> => {
-
     // Development configuration
-    const configDevelopment$ = http
-      .get('assets/config.json')
-      .pipe(
-        delay(300),
-        catchError((x: HttpErrorResponse, _): Observable<any> => {
+    const configDevelopment$ = http.get('assets/config.json').pipe(
+      catchError(
+        (x: HttpErrorResponse, _): Observable<any> => {
           console.error('Development config file not found on assets folder!', x.status, x.statusText);
           return of({});
-        })
-      );
+        }
+      )
+    );
 
     // Production configuration
-    const configProduction$ = environment.production === false ?
-      of({}) :
-      http
-        .get('assets/config.prod.json')
-        .pipe(
-          catchError((x: HttpErrorResponse, _): Observable<any> => {
-            console.error('Production config file not found on assets folder!', x.status, x.statusText);
-            return of({});
-          })
-        );
+    const configProduction$ =
+      environment.production === false
+        ? of({})
+        : http.get('assets/config.prod.json').pipe(
+            catchError(
+              (x: HttpErrorResponse, _): Observable<any> => {
+                console.error('Production config file not found on assets folder!', x.status, x.statusText);
+                return of({});
+              }
+            )
+          );
 
     return new Promise<boolean>((resolve: (a: boolean) => void): void => {
       forkJoin(configDevelopment$, configProduction$)
         .pipe(
-          delay(1200),
+          delay(environment.production ? 500 : 100),
           map(([configLocal, configProd]) => {
             configuration.setData({ ...configLocal, ...configProd });
             resolve(true);
           }),
-          catchError((x: HttpErrorResponse, _): Observable<any> => {
-            console.error('Failed to read configuration!', x.status, x.statusText);
-            return of({});
-          })
+          catchError(
+            (x: HttpErrorResponse, _): Observable<any> => {
+              console.error('Failed to read configuration!', x.status, x.statusText);
+              return of({});
+            }
+          )
         )
         .subscribe();
     });
